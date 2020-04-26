18222ebf00493900c96b3e9e12db1b47aaba0a0a
Stepan Polevshchikov <spolevshchikov@ozon.ru>
2020-04-26 23:09:07 +0300

    web: MySwitch -> ts

--- web/src/components/routes/MySwitch.tsx
+++ web/src/components/routes/MySwitch.tsx
@@ -1,33 +1,34 @@
 import React, { Component } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
-import { connect } from "react-redux";
+import { connect, ConnectedProps } from "react-redux";
 
 import StartPage from '../StartPage/StartPage'
 import HistoryPage from '../HistoryPage/HistoryPage'
 import SettingsPage from '../SettingsPage/SettingsPage'
 import DetailsPage from '../DetailsPage/DetailsPage'
+import { StoreTypes } from "../../store";
 
-class MySwitch extends Component {
+const mapStateToProps = (state: StoreTypes) => {
+	return {
+		isSettings: state.settings.conf.repoName
+	}
+}
+
+const connector = connect(mapStateToProps)
+
+type PropsFromRedux = ConnectedProps<typeof connector>
+
+class MySwitch extends Component<PropsFromRedux> {
 	render() {
 		const isSettings = !!this.props.isSettings
 		return (
 			<Switch>
 				<Route exact path="/" component={!isSettings ? StartPage : HistoryPage} />
 				<Route path="/settings" component={SettingsPage} />
-				{/* TODO: /build/id 
-					сделаем еще один endpoint для поиска по номеру на стороне сервера
-					все равно ходить за свежей инфой всегда. 
-				*/}
 				<Route path="/build/:number" component={DetailsPage} />
 			</Switch>
 		);
 	}
 }
 
-const mapStateToProps = (state) => {
-	return {
-		isSettings: state.settings.conf.repoName
-	}
-}
-
-export default connect(mapStateToProps)(MySwitch);
+export default connector(MySwitch);
