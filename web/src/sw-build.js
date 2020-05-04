c43003469c44485b8451fa5a12889adb34471d31
Stepan Polevshchikov <spolevshchikov@ozon.ru>
2020-05-04 20:34:45 +0300

    add html, svg to precached. js,css = NetworkFirst.

--- web/src/sw-build.js
+++ web/src/sw-build.js
@@ -2,18 +2,16 @@
 
 const workboxBuild = require('workbox-build');
 
-// NOTE: This should be run *AFTER* all your assets are built
+
 const buildSW = () => {
-	// This will return a Promise
 	return workboxBuild.injectManifest({
 		swSrc: 'src/sw-template.js',
 		swDest: 'build/sw.js',
 		globDirectory: 'build',
 		globPatterns: [
-			'**\/!(service-worker|precache-manifest.*).{js,css,html,png}',
+			'**\/!(service-worker|precache-manifest.*).{html,svg}',
 		],
 	}).then(({ count, size, warnings }) => {
-		// Optionally, log any warnings and details.
 		warnings.forEach(console.warn);
 		console.log(`${count} files will be precached, totaling ${size} bytes.`);
 	});
--- web/src/sw-template.js
+++ web/src/sw-template.js
@@ -1,42 +1,67 @@
-// custom cache rules go here
+if ('function' === typeof importScripts) {
+	importScripts(
+		'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js'
+	);
 
-// if ('function' === typeof importScripts) {
-importScripts(
-	'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js'
-);
+	// self.addEventListener('install', (event) => {
+	// 	console.log('fire1')
+	// });
 
-// add for skipWaiting
-addEventListener('message', (event) => {
-	console.log('RUN SIPWAITING')
-	if (event.data && event.data.type === 'SKIP_WAITING') {
-		skipWaiting();
+	// self.addEventListener('activate', (event) => {
+	// 	console.log('fire2')
+	// });
+
+	// self.addEventListener('fetch', (event) => {
+	// 	console.log('fire3')
+	// });
+
+	if (workbox) {
+		console.log('Workbox is loaded');
+
+		workbox.core.skipWaiting();
+		workbox.core.clientsClaim();
+
+		workbox.core.setCacheNameDetails({
+			prefix: 'ci-server-svg',
+			suffix: 'v0.1'
+		});
+
+		workbox.precaching.cleanupOutdatedCaches()
+
+		workbox.precaching.precache(self.__WB_MANIFEST);
+
+		/* custom cache rules*/
+		// workbox.routing.registerNavigationRoute('/index.html', {
+		// 	blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/],
+		// });
+
+		workbox.routing.registerRoute(
+			new RegExp('/static/'),
+			new workbox.strategies.NetworkFirst({
+				cacheName: "static-resources_v2",
+				plugins: [
+					new workbox.expiration.Plugin({
+						maxEntries: 60,
+						maxAgeSeconds: 1 * 24 * 60 * 60, // 1 Days
+					}),
+				],
+			})
+		);
+
+		workbox.routing.registerRoute(
+			/\.(?:png)$/,
+			new workbox.strategies.NetworkFirst({
+				cacheName: "images-resources_v2",
+				plugins: [
+					new workbox.expiration.Plugin({
+						maxEntries: 60,
+						maxAgeSeconds: 1 * 24 * 60 * 60, // 1 Days
+					}),
+				],
+			})
+		);
+
+	} else {
+		console.log('Workbox could not be loaded. No Offline support');
 	}
-});
-
-console.log('Workbox is loaded');
-
-/* injection point for manifest files.  */
-workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
-
-/* custom cache rules*/
-// workbox.routing.registerNavigationRoute('/index.html', {
-// 	blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/],
-// });
-
-workbox.routing.registerRoute(
-	/\.(?:js|svg)$/,
-	workbox.strategies.cacheFirst({
-		cacheName: 'MY_JS_For_AUDIT',
-		plugins: [
-			new workbox.expiration.Plugin({
-				maxEntries: 60,
-				maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-			}),
-		],
-	})
-);
-
-	// } else {
-	// 	console.log('Workbox could not be loaded. No Offline support');
-	// }
-// }
+}
